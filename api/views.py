34729232745608954afbfc6dd55d6a3129294d21
Hillary Chepsergon <hillergon@gmail.com>
2020-06-03 10:03:22 +0300

    added api view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ProductsApiview(APIView):
+
+    def get(self,request,format=None):
+        an_apiview = [
+            'Oranges',
+            'Mangoes',
+            'Banana'
+
+        ]
+
+        return Response({'message':'continental fruits','an_apiview':an_apiview})
+
+
